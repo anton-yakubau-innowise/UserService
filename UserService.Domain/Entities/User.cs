4c9bc6566156612aa3524e045087bf37a967fc6e
Anton Jakubau <170anton@gmail.com>
2025-06-30 12:25:28 +0200

    Add User

---
+++ UserService.Domain/Entities/User.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserService.Domain.Entities
+{
+    public class User : IdentityUser<Guid>
+    {
+        [Required]
+        public string FirstName { get; private set; }
+
+        [Required]
+        public string LastName { get; private set; }
+
+        [Required]
+        public DateTime CreatedAt { get; private set; }
+
+        public DateTime? UpdatedAt { get; private set; }
+
+        private User()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            UserName = string.Empty;
+            Email = string.Empty;
+        }
+
+        public static User RegisterNew(string email, string username, string firstName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(email)) throw new ArgumentNullException(nameof(email));
+            if (string.IsNullOrWhiteSpace(username)) throw new ArgumentNullException(nameof(username));
+
+            return new User
+            {
+                Id = Guid.NewGuid(),
+                Email = email,
+                UserName = username,
+                FirstName = firstName,
+                LastName = lastName,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = null
+            };
+        }
+
+        public void UpdateProfile(string firstName, string lastName)
+        {
+            if (!string.IsNullOrWhiteSpace(firstName))
+            {
+                FirstName = firstName;
+            }
+
+            if (!string.IsNullOrWhiteSpace(lastName))
+            {
+                LastName = lastName;
+            }
+
+            SetUpdated();
+        }
+
+        private void SetUpdated()
+        {
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
