d7117c0d4e826e58f8c1da9758497ae1db1324c5
Anton Jakubau <170anton@gmail.com>
2025-07-04 14:06:37 +0200

    Configure MongoDB

--- UserService.Domain/Entities/User.cs
+++ UserService.Domain/Entities/User.cs
@@ -1,30 +1,30 @@
-using Microsoft.AspNetCore.Identity;
-using System.ComponentModel.DataAnnotations;
 using UserService.Domain.Common;
+using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository.Attributes;
 
 namespace UserService.Domain.Entities
 {
-    public class User : IdentityUser<Guid>
+    [CollectionName("Users")]
+    public class User : MongoIdentityUser<Guid>
     {
-        [Required]
+
         public string FirstName { get; private set; }
 
-        [Required]
         public string LastName { get; private set; }
 
-        [Required]
         public DateTime CreatedAt { get; private set; }
 
         public DateTime? UpdatedAt { get; private set; }
-
-        private User()
+    
+        public User() : base()
         {
             FirstName = string.Empty;
             LastName = string.Empty;
             UserName = string.Empty;
             Email = string.Empty;
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = null;
         }
-
         public static User RegisterNewUser(string email, string username, string firstName, string lastName)
         {
             Guard.AgainstInvalidEmail(email);
--- UserService.Infrastructure/InfrastructureServiceExtensions.cs
+++ UserService.Infrastructure/InfrastructureServiceExtensions.cs
@@ -1,23 +1,48 @@
-using Microsoft.EntityFrameworkCore;
+using AspNetCore.Identity.MongoDbCore.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using UserService.Application.Interfaces;
+using MongoDB.Driver;
+using UserService.Domain.Entities;
 using UserService.Domain.Repositories;
-using UserService.Infrastructure.Persistence;
 using UserService.Infrastructure.Persistence.Repositories;
 
-namespace UserService.Infrastructure;
+namespace UserService.Infrastructure
+{
+    public static class InfrastructureServiceExtensions
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["MongoDbSettings:ConnectionString"];
+            var databaseName = configuration["MongoDbSettings:DatabaseName"];
 
-// public static class InfrastructureServiceExtensions
-// {
-//     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
-//     {
-//         services.AddDbContext<UserDbContext>(options =>
-//             options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+            if (string.IsNullOrEmpty(connectionString) || string.IsNullOrEmpty(databaseName))
+            {
+                throw new InvalidOperationException("ConnectionString or DatabaseName for MongoDB are not set in the configuration.");
+            }
 
-//         services.AddScoped<IVehicleRepository, VehicleRepository>();
-//         services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddIdentity<User, MongoIdentityRole<Guid>>(options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequiredLength = 4;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = false;
+                })
+                .AddMongoDbStores<User, MongoIdentityRole<Guid>, Guid>(connectionString, databaseName)
+                .AddSignInManager()
+                .AddDefaultTokenProviders();
 
-//         return services;
-//     }
-// }
+            services.AddSingleton<IMongoClient>(sp => new MongoClient(connectionString));
+            services.AddScoped(sp =>
+            {
+                var client = sp.GetRequiredService<IMongoClient>();
+                return client.GetDatabase(databaseName);
+            });
+
+            services.AddScoped<IUserRepository, MongoUserRepository>();
+            
+            return services;
+        }
+    }
+}
